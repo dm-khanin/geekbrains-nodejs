345c72e11f2beb13f8e5b2a487f289f7b6df68c7
dmitry-khanin <dm.khanin@gmail.com>
2017-11-01 21:02:34 +0300

    task 1 done

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var _news = require('./news');
+
+var _news2 = _interopRequireDefault(_news);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _news2.default)();
