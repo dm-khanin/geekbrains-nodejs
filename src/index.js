6dda43f61d936e477cb6a882d11971fe8fe4918c
dmitry-khanin <dm.khanin@gmail.com>
2017-11-06 17:04:03 +0300

    task 2 done

--- src/index.js
+++ src/index.js
@@ -4,11 +4,21 @@ import consolidate from 'consolidate';
 import bodyParser from 'body-parser';
 import template from 'consolidate';
 import feed from 'rss-to-json';
+import cookieSession from 'cookie-session';
+import cookieParser from 'cookie-parser';
+import path from 'path';
 
 const logger = Logger();
 const app = express();
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(cookieSession({
+    name: 'session',
+    keys: ['secretKey', 'site', 'format', 'count'],
+    maxAge: 24 * 60 * 60 * 1000,
+}));
+
 app.engine('hbs', template.handlebars);
 app.set('view engine', 'hbs');
 app.set('views', `${__dirname}/views`);
@@ -27,6 +37,9 @@ const makeTxtFileString = (json, newsCount) => {
 app.post('/feed', (req, res) => {
     try {
         const { site, format, count } = req.body;
+        req.session.format = format;
+        req.session.site = site;
+        req.session.count = count;
         feed.load(site, (err, rss) => {
             if (err) throw err;
             if (format === 'txt') {
